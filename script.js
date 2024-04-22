76f0bda03c345c8c342fb90a9def58f8f12e46a7
Sergii Marchenko <marikmarchelo@gmail.com>
2024-04-22 20:37:28 +0300

    new commit

--- script.js
+++ script.js
@@ -8,7 +8,17 @@ const eventScrollCounter = {
   throttled: 0,
 };
 
-document.addEventListener("scroll", function (e) {
+function handleScroll() {
+  console.log("kkk");
   eventScrollCounter.standart += 1;
   refs.outputScrollStandart.textContent = eventScrollCounter.standart;
-});
+}
+
+function handleScrollThrottle() {
+  console.log("jjj");
+  eventScrollCounter.throttled += 1;
+  refs.outputScrollThrottled.textContent = eventScrollCounter.throttled;
+}
+
+window.addEventListener("scroll", handleScroll);
+window.addEventListener("scroll", _.throttle(handleScrollThrottle, 300));
